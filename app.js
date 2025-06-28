7c6b455fa02d6c8f9ae698a976c43662cd93dc81
Aman Chaurasiya <amanc2804@gmail.com>
2025-06-28 23:46:41 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var MenyItems = document.getElementById("MenuItmes");
+MenuItmes.style.maxHight = "0px";
+function menutoggle(){
+    if(MenuItmes.style.maxHight == "0px"){
+        MenuItmes.style.maxHight = "200px";
+    }else{
+        MenuItmes.style.maxHight = "0px";
+    }
+}
+
+
